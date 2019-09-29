a9a9bb69fe98a305daaa0ecf2a6d838f68d1921c
mohamedhssien <moha.abdi1771@gmail.com>
2019-09-29 19:10:19 +0300

    add some comments

--- classes/LevenshteinClass.php
+++ classes/LevenshteinClass.php
@@ -9,7 +9,7 @@ class Levenshtein {
         $this->string1 = $string1;
         $this->string2 = $string2;
     }
-
+    //func that 
     public function getDistance(){
         if(strlen($this->string1) == 0 ){ 
             return strlen($this->string2);
@@ -17,21 +17,25 @@ class Levenshtein {
         if(strlen($this->string2) == 0 ){
              return strlen($this->string1);
         }
-
+        //fill first column in each row
         for($i = 0; $i <= strlen($this->string2); $i++){
             $this->matrix[$i] = [$i];
         }
+        //fill first row in each column
         for($j = 0; $j <= strlen($this->string1); $j++){
             $this->matrix[0][$j] = $j;
         }
 
+        //fill the matrix by considering three posible things
+        //if tow characters are same we copy the diagonal element.
+        //otherwise we take minimum of left diagonal and up elements and add one 
         for($i = 1; $i <= strlen($this->string2); $i++){
             for($j = 1; $j <= strlen($this->string1); $j++){
                 if($this->string2[$i-1] == $this->string1[$j-1]){
                     $this->matrix[$i][$j] = $this->matrix[$i-1][$j-1]; 
                 }else{
-                    $this->matrix[$i][$j] = min($this->matrix[$i-1][$j-1] + 1, 
-                                                min($this->matrix[$i][$j-1] + 1,
+                    $this->matrix[$i][$j] = min($this->matrix[$i-1][$j-1] + 1, //substutation 
+                                                min($this->matrix[$i][$j-1] + 1, //insertion
                                                     $this->matrix[$i-1][$j] + 1)); // deletion
                     //$this->matrix[$i][$j] = [5][1];
                 }
--- classes/hammingClass.php
+++ classes/hammingClass.php
@@ -1,16 +1,16 @@
 <?php
 
 class Hamming {
-
-    public $string1;
-    public $string2;
+    //attributes
+    private $string1;
+    private $string2;
     public $distance = 0;
-
+    //the constructor accept two arguments
     public function __construct($string1, $string2){
         $this->string1 = $string1;
         $this->string2 = $string2;
     }
-
+    //func that check different between strings and increase distance if there is mismatch
     public function getDistance(){
         if( strlen($this->string1) != strlen($this->string2)){
             echo "not same length";
